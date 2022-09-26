46fbce26e363edc7f160024fdf74d700804a3a76
samantrs <https://samantrs@git.rabobank.nl>
2022-09-26 23:18:53 +0200

    close account event fix

--- bank-account-common/src/main/java/com/eventsourcing/cqrs/bankaccountcommon/events/AccountClosedEvent.java
+++ bank-account-common/src/main/java/com/eventsourcing/cqrs/bankaccountcommon/events/AccountClosedEvent.java
@@ -2,9 +2,11 @@ package com.eventsourcing.cqrs.bankaccountcommon.events;
 
 import com.eventsourcing.cqrs.bankaccountcorecqrs.events.BaseEvent;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
+@NoArgsConstructor
 @SuperBuilder
 public class AccountClosedEvent extends BaseEvent {
 
