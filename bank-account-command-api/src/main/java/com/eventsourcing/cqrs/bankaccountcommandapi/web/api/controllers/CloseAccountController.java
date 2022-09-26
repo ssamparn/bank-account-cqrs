b210b7ce05e766ce2944d2bafdda58d3fd916d38
samantrs <https://samantrs@git.rabobank.nl>
2022-09-26 22:55:04 +0200

    close account rest controller

--- bank-account-command-api/src/main/java/com/eventsourcing/cqrs/bankaccountcommandapi/web/api/controllers/CloseAccountController.java
+++ bank-account-command-api/src/main/java/com/eventsourcing/cqrs/bankaccountcommandapi/web/api/controllers/CloseAccountController.java
@@ -1,6 +1,6 @@
 package com.eventsourcing.cqrs.bankaccountcommandapi.web.api.controllers;
 
-import com.eventsourcing.cqrs.bankaccountcommandapi.web.api.commands.WithdrawFundsCommand;
+import com.eventsourcing.cqrs.bankaccountcommandapi.web.api.commands.CloseAccountCommand;
 import com.eventsourcing.cqrs.bankaccountcommon.dto.response.BaseResponse;
 import com.eventsourcing.cqrs.bankaccountcorecqrs.exceptions.AggregateNotFoundException;
 import com.eventsourcing.cqrs.bankaccountcorecqrs.infrastructure.CommandDispatcher;
@@ -8,9 +8,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,23 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/api/v1")
 @RequiredArgsConstructor
-public class WithdrawFundsRestController {
+public class CloseAccountController {
 
     private final CommandDispatcher commandDispatcher;
 
-    @PutMapping("/withdraw-funds/{accountId}")
-    public ResponseEntity<BaseResponse> withdrawFunds(@PathVariable String accountId,
-                                                    @RequestBody WithdrawFundsCommand withdrawFundsCommand) {
-        withdrawFundsCommand.setId(accountId);
-
+    @DeleteMapping("/close-bank-account/{accountId}")
+    public ResponseEntity<BaseResponse> closeAccount(@PathVariable String accountId) {
         try {
-            commandDispatcher.send(withdrawFundsCommand);
-            return new ResponseEntity<>(new BaseResponse("Withdraw funds request completed successfully"), HttpStatus.OK);
+            commandDispatcher.send(new CloseAccountCommand(accountId));
+            return new ResponseEntity<>(new BaseResponse("Bank account closure request completed successfully"), HttpStatus.OK);
         } catch (IllegalStateException | AggregateNotFoundException e) {
             log.warn("Client made a BAD REQUEST: {}", e.getMessage());
             return new ResponseEntity<>(new BaseResponse(e.toString()), HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
-            log.error("Error while processing request to withdraw funds from the account with accountId : {}, {}", accountId, e.getMessage());
+            log.error("Error while processing request to close bank account with accountId : {}, {}", accountId, e.getMessage());
             return new ResponseEntity<>(new BaseResponse(e.toString()), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
